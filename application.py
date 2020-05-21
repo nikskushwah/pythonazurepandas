d0968df34b5f52e2216dc4488ab5fdd8728140ef
nikskushwah <nikskushwah@gmail.com>
2020-05-21 16:24:07 -0500

    Update application.py

--- application.py
+++ application.py
@@ -9,4 +9,5 @@ app = Flask(__name__)
 def home():
     s = pd.Series([1, 3, 5, np.nan, 6, 8])
 #    dates = pd.date_range('20130101', periods=6)
-    return s
+    print(s)
+    return "Hello World"
